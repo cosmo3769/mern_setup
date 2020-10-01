dbf9aa9e5b9bcc13399588b1a94505017e4382b5
piyush-cosmo <53268607+piyush-cosmo@users.noreply.github.com>
2020-10-02 00:29:28 +0530

    serving static files from dist folder

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import devBundle from './devBundle';
+import path from 'path';
+const CURRENT_WORKING_DIR = process.cwd();
 
 const app = express();
 devBundle.compile(app);
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
