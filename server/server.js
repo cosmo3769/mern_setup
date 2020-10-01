a5bcc62145c6f70b82296a13b4374297fe204aa3
piyush-cosmo <53268607+piyush-cosmo@users.noreply.github.com>
2020-10-02 00:51:00 +0530

    server listening

--- server/server.js
+++ server/server.js
@@ -15,3 +15,12 @@ app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 app.get('/', (req, res) => {
   res.status(200).send(template())
 })
+
+//listen to my server bro
+let port = process.env.PORT || 3000
+app.listen(port, function onStart(err) {
+  if (err) {
+    console.log(err)
+  }
+  console.info('Server started on port %s.', port)
+})
